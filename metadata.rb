18c714ed6bb17b8d6f049b9419596f96f2aa1b74
Tim Smith <tsmith84@gmail.com>
2015-10-21 12:05:39 -0700

    Add long description to metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides the mysql_chef_gem resource'
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.2'
 
 
